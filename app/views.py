6d95a36ee31d75e2f75952d8e02237b8de5bb813
Creation Yun <creationyun@gmail.com>
2018-09-29 12:11:15 +0900

    Add testing input values in the web service.

--- app/views.py
+++ app/views.py
@@ -568,10 +568,43 @@ def web_index(request):
 #################***************** 요청 페이지 *****************#################
 def web_guide_request(request):
     content = {}
+
+    # POST 방식이라면 (즉, 요청 신청 버튼을 누른 상태라면)
     if request.method == 'POST':
+        content['req_try'] = True
+        content['success'] = True
+        content['req_message'] = '요청이 완료되었습니다. 잠시만 기다려주세요!'
+
+        # 데이터 로드
         name = request.POST['input_name']
         start = request.POST['input_startpoint']
         dest = request.POST['input_destination']
-        WebGuideRequests.objects.create(user=name, startpoint=start, destination=dest)
-        content['success'] = True
+
+        # 입력값 검사
+        if len(name) <= 0:
+            content['success'] = False
+            content['req_message'] = '이름을 입력해주세요.'
+        elif len(name) >= 100:
+            content['success'] = False
+            content['req_message'] = '이름은 100자 이상 입력할 수 없습니다.'
+        elif len(start) <= 0:
+            content['success'] = False
+            content['req_message'] = '출발지를 입력해주세요.'
+        elif len(start) >= 100:
+            content['success'] = False
+            content['req_message'] = '출발지는 100자 이상 입력할 수 없습니다.'
+        elif len(dest) <= 0:
+            content['success'] = False
+            content['req_message'] = '목적지를 입력해주세요.'
+        elif len(dest) >= 100:
+            content['success'] = False
+            content['req_message'] = '목적지는 100자 이상 입력할 수 없습니다.'
+
+        # 검사 통과시 DB(요청목록)에 등록
+        if content['success']:
+            WebGuideRequests.objects.create(
+                user=name, startpoint=start, destination=dest
+            )
+
+    # 페이지 렌더링
     return render(request, 'web/guide_request.html', content)
