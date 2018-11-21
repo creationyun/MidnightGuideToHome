41614cfaad6d3c4e1057ee1d5c79144e47f0f5f4
Ubuntu <ubuntu@ip-172-31-19-180.ap-northeast-2.compute.internal>
2018-11-21 11:15:17 +0900

    Fix include error and make new migration

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import KakaoService
+from .models import KakaoService, WebGuideRequests, WebGuideReplies
 
 # Register your models here.
 admin.site.register(KakaoService)
