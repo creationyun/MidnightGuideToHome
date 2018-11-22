6831c104293aa521856e0edd23bd199ec2e460fb
Creation Yun <creationyun@gmail.com>
2018-11-23 00:14:30 +0900

    Fix bug of request details

--- app/web_urls.py
+++ app/web_urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', web_index),
     path('guide_request/', web_guide_request),
     path('request_view/<slug:condition>/', web_request_view),
-    path('request_view/<int:req_id>/', web_request_detail),
+    path('request_view/detail/<int:req_id>/', web_request_detail),
 ]
