811e6a6998aaadc7877c1c6dd6a75cf3e3ab29ee
creationyun <creationyun@outlook.com>
2020-08-26 23:32:20 +0900

    Add departure time of Google Maps

--- app_kakaoi/views.py
+++ app_kakaoi/views.py
@@ -5,19 +5,33 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 import urllib
 import requests
+from datetime import datetime, timedelta
 from static_lib import MidnightGuideResultKakao
 # tokens.py는 보안상 github에 업로드하지 않았다.
 from tokens import API_KEY
 
 # API 버전
 ver = "2.0"
+# 비정상적 반응
+abnormal_response = {
+    "version": ver,
+    "template": {
+        "outputs": [
+            {
+                "simpleText": {
+                    "text": "요청이 비정상적입니다. 관리자에게 문의해주세요."
+                }
+            }
+        ]
+    }
+}
 
 #################***************** 경로 찾기 *****************#################
 @csrf_exempt
 def kakaoi_findpath(request):
     # 올바른 POST 요청이 맞는지 검증
     if request.method != 'POST':
-        return HttpResponse('Wrong request.')
+        return JsonResponse(abnormal_response)
 
     # 카카오 서버로부터 받은 JSON request에서 데이터를 추출한다.
     json_str = (request.body).decode('utf-8')
@@ -33,18 +47,28 @@ def kakaoi_findpath(request):
         req_dest = received_json['action']['params']['location_dest']
     except KeyError:
         # 비정상적인 JSON 요청
-        return JsonResponse({
-            "version": ver,
-            "template": {
-                "outputs": [
-                    {
-                        "simpleText": {
-                            "text": "요청이 비정상적입니다. 관리자에게 문의해주세요."
-                        }
-                    }
-                ]
-            }
-        })
+        return JsonResponse(abnormal_response)
+
+    try:
+        if req_time == '현재 시간':
+            # 현재 시간일 경우
+            departure_datetime = datetime.now()
+        else:
+            # 시간 포맷팅 및 현재 날짜에 반영
+            departure_time = datetime.strptime(req_time, "%H:%M")
+            departure_datetime = datetime.now()
+            
+            departure_datetime = departure_datetime.replace(hour=departure_time.hour)
+            departure_datetime = departure_datetime.replace(minute=departure_time.minute)
+            departure_datetime = departure_datetime.replace(second=0)
+            departure_datetime = departure_datetime.replace(microsecond=0)
+            
+            # 현재 시간과 반영된 시간 비교 - 과거면 하루 추가
+            if departure_datetime < datetime.now():
+                departure_datetime += timedelta(days=1)
+    except ValueError:
+        # 비정상적인 JSON 요청
+        return JsonResponse(abnormal_response)
 
     # 구글 지도 API 파라미터
     maps_api_url = 'https://maps.googleapis.com/maps/api/directions/json'
@@ -53,6 +77,7 @@ def kakaoi_findpath(request):
         "destination": req_dest,
         "language": "ko",
         "region": "kr",
+        "departure_time": int(departure_datetime.timestamp()),
         "mode": "transit",
         "key": API_KEY
     }
@@ -60,22 +85,26 @@ def kakaoi_findpath(request):
     # 구글 지도 API (GET) 요청 및 JSON Parsing
     r = requests.get(url=maps_api_url, params=maps_api_param)
     google_maps_json = r.json()
-    text = ''
+    path_result = ''
     
-    # 모든 route와 leg, step에 대해 경로 추출
-    for route in google_maps_json["routes"]:
-        for leg in route["legs"]:
-            for step in leg["steps"]:
-                if step["travel_mode"] == "WALKING":
-                    # 도보
-                    text += step["html_instructions"] + '\n'
-                elif step["travel_mode"] == "TRANSIT":
-                    # 대중교통
-                    text += '{}, {}\n'.format(
-                        step["transit_details"]["line"]["short_name"],
-                        step["html_instructions"]
-                    )
-
+    try:
+        # 모든 route와 leg, step에 대해 경로 추출
+        for route in google_maps_json["routes"]:
+            for leg in route["legs"]:
+                for step in leg["steps"]:
+                    if step["travel_mode"] == "WALKING":
+                        # 도보
+                        path_result += step["html_instructions"] + '\n'
+                    elif step["travel_mode"] == "TRANSIT":
+                        # 대중교통
+                        path_result += '{}, {}\n'.format(
+                            step["transit_details"]["line"]["short_name"],
+                            step["html_instructions"]
+                        )
+    except KeyError:
+        # 비정상적인 JSON 요청
+        return JsonResponse(abnormal_response)
+        
     # 구글 지도 URL 파라미터 (non-API)
     google_maps_param = {
         "api": "1",
@@ -92,7 +121,7 @@ def kakaoi_findpath(request):
                 {
                     "simpleText": {
                         "text": MidnightGuideResultKakao.format(
-                            req_origin, req_dest, req_time, text,
+                            req_origin, req_dest, req_time, path_result,
                             urllib.parse.urlencode(google_maps_param)
                         )
                     }
