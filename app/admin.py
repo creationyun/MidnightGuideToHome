5c928084de0fecb4205036a8964f2d300103c21d
Creation Yun <36063812+creationyun@users.noreply.github.com>
2020-09-10 13:59:01 +0900

    Migrate to delete KakaoService

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import KakaoService, WebGuideRequests, WebGuideReplies
+from .models import WebGuideRequests, WebGuideReplies
 
 # Register your models here.
-admin.site.register(KakaoService)
 admin.site.register(WebGuideRequests)
 admin.site.register(WebGuideReplies)
