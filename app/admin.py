92a5f5d011483aa0e39d8619db71ac0b9efa4a72
Creation Yun <36063812+creationyun@users.noreply.github.com>
2021-05-12 16:10:54 +0900

    Update admin.py - add WebPubTransRoutesComparisons

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import WebGuideRequests, WebGuideReplies
+from .models import WebGuideRequests, WebGuideReplies, WebPubTransRoutesComparisons
 
 # Register your models here.
 admin.site.register(WebGuideRequests)
 admin.site.register(WebGuideReplies)
+admin.site.register(WebPubTransRoutesComparisons)
